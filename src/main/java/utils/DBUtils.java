d65c899132b6b908b37f1f0310130e8777703c83
mdiansow <mdiansow@yahoo.fr>
2016-02-27 20:49:25 +0100

    log files

--- src/main/java/utils/DBUtils.java
+++ src/main/java/utils/DBUtils.java
@@ -22,33 +22,39 @@ public class DBUtils {
 	private static final Logger dbUtilsLOGGER = org.apache.log4j.Logger
 			.getLogger(DBUtils.class);
 
-	private final static String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+	private final static String DB_URL = "jdbc:postgresql://localhost:5432/";
 
-	private final static String DB_NAME = "postgres";
+	private final static String DB_NAME = "dvdrental";
+
+	private final static String DB_USER = "postgres";
 
 	private final static String DB_PASSWORD = "2182";
 
 	private final static String DB_DRIVER = "org.postgresql.Driver";
 
+	private static Connection conn = null;
+
 	public static Connection getConnection(Logger logger) throws SQLException {
-		
+
 		dbUtilsLOGGER.setLevel(Level.INFO);
 		PatternLayout layout = new PatternLayout("%d %-5p %F:%L - %m%n");
 
 		try {
-			dbUtilsLOGGER
-					.addAppender(new FileAppender(layout, "stats_logger.log"));
+			dbUtilsLOGGER.addAppender(new FileAppender(layout,
+					"stats_logger.log"));
 		} catch (IOException e) {
 			dbUtilsLOGGER.error(
 					"[main] Erreur lors de l'ouverture du fichier de log", e);
 			e.printStackTrace();
-		} 
-		Connection conn = null;
+		}
 		// enregistrer le pilote
 		try {
 			Class.forName(DB_DRIVER);
 			// String url = DB_URL;
-			conn = DriverManager.getConnection(DB_URL, DB_NAME, DB_PASSWORD);
+			dbUtilsLOGGER.info("Connexion à la base de données "
+					+ DB_USER.toUpperCase());
+			conn = DriverManager.getConnection(DB_URL + DB_NAME, DB_USER,
+					DB_PASSWORD);
 			dbUtilsLOGGER.info("Connexion réussi...");
 
 		} catch (ClassNotFoundException e) {
@@ -63,4 +69,12 @@ public class DBUtils {
 		return conn;
 	}
 
+	public static void closeConnection(Logger logger) throws SQLException {
+		try {
+			conn.close();
+			dbUtilsLOGGER.info("Déconnexion réussi...");
+		} catch (Exception e) {
+			dbUtilsLOGGER.error(e.getMessage());
+		}
+	}
 }
